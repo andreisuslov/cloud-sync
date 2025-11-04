4f27a40d50f0cf4cbba0ea2c594df3ae164bed16
andreisuslov <truvord@gmail.com>
2025-11-03 22:32:49 -0500

    test(installation): skip tests for removed menu items

--- tests/unit/installation_menu_test.go
+++ tests/unit/installation_menu_test.go
@@ -39,122 +39,28 @@ func TestMainInstallationMenu(t *testing.T) {
 	updatedModel, _ := model.Update(tea.WindowSizeMsg{Width: 80, Height: 30})
 	model = updatedModel.(views.InstallationModel)
 	
-	// Test main menu rendering
+	// Test main menu rendering - should be empty now
 	view := model.View()
 	if !strings.Contains(view, "Installation Menu") {
 		t.Error("Main menu should display 'Installation Menu'")
 	}
-	if !strings.Contains(view, "Install and set up required tools") {
-		t.Error("Main menu should display option 1")
-	}
-	if !strings.Contains(view, "Set up a new location") {
-		t.Error("Main menu should display option 2")
-	}
-	if !strings.Contains(view, "View existing locations") {
-		t.Error("Main menu should display option 3")
-	}
+	// Installation menu is now empty - no items to check
 }
 
 func TestLocationTypeSelection(t *testing.T) {
-	model := views.NewInstallationModel()
-	updatedModel, _ := model.Update(tea.WindowSizeMsg{Width: 80, Height: 24})
-	model = updatedModel.(views.InstallationModel)
-	
-	// Navigate down to "Set up a new location" and select it
-	updatedModel, _ = model.Update(tea.KeyMsg{Type: tea.KeyDown})
-	model = updatedModel.(views.InstallationModel)
-	updatedModel, _ = model.Update(tea.KeyMsg{Type: tea.KeyEnter})
-	model = updatedModel.(views.InstallationModel)
-	
-	// Test location type selection rendering
-	view := model.View()
-	if !strings.Contains(view, "Set Up a New Location") {
-		t.Error("Should display 'Set Up a New Location' heading")
-	}
-	if !strings.Contains(view, "Local folder") {
-		t.Error("Should display 'Local folder' option")
-	}
-	if !strings.Contains(view, "Remote storage") {
-		t.Error("Should display 'Remote storage' option")
-	}
+	t.Skip("Installation menu is now empty - test no longer applicable")
 }
 
 func TestRemoteLocationSelection(t *testing.T) {
-	model := views.NewInstallationModel()
-	updatedModel, _ := model.Update(tea.WindowSizeMsg{Width: 80, Height: 24})
-	model = updatedModel.(views.InstallationModel)
-	
-	// Navigate to "Set up a new location" and select it
-	updatedModel, _ = model.Update(tea.KeyMsg{Type: tea.KeyDown})
-	model = updatedModel.(views.InstallationModel)
-	updatedModel, _ = model.Update(tea.KeyMsg{Type: tea.KeyEnter})
-	model = updatedModel.(views.InstallationModel)
-	
-	// Navigate to "Remote storage" and select it
-	updatedModel, _ = model.Update(tea.KeyMsg{Type: tea.KeyDown})
-	model = updatedModel.(views.InstallationModel)
-	updatedModel, _ = model.Update(tea.KeyMsg{Type: tea.KeyEnter})
-	model = updatedModel.(views.InstallationModel)
-	
-	// Test remote location selection rendering
-	view := model.View()
-	if !strings.Contains(view, "Configure Remote Storage") {
-		t.Error("Should display 'Configure Remote Storage' heading")
-	}
-	if !strings.Contains(view, "Backblaze B2") {
-		t.Error("Should display 'Backblaze B2' option")
-	}
-	if !strings.Contains(view, "Amazon S3") {
-		t.Error("Should display 'Amazon S3' option")
-	}
+	t.Skip("Installation menu is now empty - test no longer applicable")
 }
 
 func TestViewExistingLocations(t *testing.T) {
-	model := views.NewInstallationModel()
-	updatedModel, _ := model.Update(tea.WindowSizeMsg{Width: 80, Height: 24})
-	model = updatedModel.(views.InstallationModel)
-	
-	// Navigate to "View existing locations" and select it
-	updatedModel, _ = model.Update(tea.KeyMsg{Type: tea.KeyDown})
-	model = updatedModel.(views.InstallationModel)
-	updatedModel, _ = model.Update(tea.KeyMsg{Type: tea.KeyDown})
-	model = updatedModel.(views.InstallationModel)
-	updatedModel, _ = model.Update(tea.KeyMsg{Type: tea.KeyEnter})
-	model = updatedModel.(views.InstallationModel)
-	
-	// Test existing locations view rendering
-	view := model.View()
-	if !strings.Contains(view, "Existing Locations") {
-		t.Error("Should display 'Existing Locations' heading")
-	}
-	if !strings.Contains(view, "Remote Locations:") {
-		t.Error("Should display 'Remote Locations:' section")
-	}
-	if !strings.Contains(view, "Local Sync Folders:") {
-		t.Error("Should display 'Local Sync Folders:' section")
-	}
+	t.Skip("Installation menu is now empty - test no longer applicable")
 }
 
 func TestBackNavigation(t *testing.T) {
-	model := views.NewInstallationModel()
-	updatedModel, _ := model.Update(tea.WindowSizeMsg{Width: 80, Height: 24})
-	model = updatedModel.(views.InstallationModel)
-	
-	// Navigate to "Set up a new location" and select it
-	updatedModel, _ = model.Update(tea.KeyMsg{Type: tea.KeyDown})
-	model = updatedModel.(views.InstallationModel)
-	updatedModel, _ = model.Update(tea.KeyMsg{Type: tea.KeyEnter})
-	model = updatedModel.(views.InstallationModel)
-	
-	// Navigate back with 'q'
-	updatedModel, _ = model.Update(tea.KeyMsg{Type: tea.KeyRunes, Runes: []rune{'q'}})
-	model = updatedModel.(views.InstallationModel)
-	
-	// Should be back at main menu
-	view := model.View()
-	if !strings.Contains(view, "Installation Menu") {
-		t.Error("Should return to main menu after pressing 'q'")
-	}
+	t.Skip("Installation menu is now empty - test no longer applicable")
 }
 
 func TestConfigsViewRendering(t *testing.T) {
